26dffb6d1cf3428c43283ca5c72eeea00a3be9e9
broliang <18706703437@163.com>
2019-06-09 21:19:42 +0800

    Add 07_bisect_insort.py

---
+++ 02_data_structures/01_an_array_of_sequences/07_bisect_insort.py
@@ -0,0 +1,15 @@
+import bisect
+import random
+
+# Sorting is expensive, so once you have a sorted sequence, it's good to keep it that way. That is why bisect.insort was created.
+# insort(seq, item) inserts item into seq so as to keep seq in ascending order.
+
+SIZE = 7
+
+random.seed(1729)
+
+my_list = []
+for i in range(SIZE):
+    new_item = random.randrange(SIZE*2)
+    bisect.insort(my_list, new_item)
+    print('%2d ->' % new_item, my_list)
