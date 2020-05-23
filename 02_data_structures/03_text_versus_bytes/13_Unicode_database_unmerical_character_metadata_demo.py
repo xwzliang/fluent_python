901f73d6f1a1267bdc5cd0733c6313d69e646537
broliang <18706703437@163.com>
2020-05-23 13:02:05 +0800

    Fix.

--- 02_data_structures/03_text_versus_bytes/13_Unicode_database_unmerical_character_metadata_demo.py
+++ 02_data_structures/03_text_versus_bytes/13_Unicode_database_unmerical_character_metadata_demo.py
@@ -1,18 +1,22 @@
-import unicodedata
 import re
+import unicodedata
 
-re_digit = re.compile(r'\d')
+re_digit = re.compile(r"\d")
 
-sample = '1\xbc\xb2\u0969\u136b\u216b\u2466\u2480\u3285'
+sample = "1\xbc\xb2\u0969\u136b\u216b\u2466\u2480\u3285"
 
 for char in sample:
-    print('U+%04x' % ord(char),	# Code point in U+0000 format
-            char.center(6),	# Character centralized in a str of length 6
-            're_dig' if re_digit.match(char) else '-',
-            'isdig' if char.isdigit() else '-',
-            'isnum' if char.isnumeric() else '-',
-            format(unicodedata.numeric(char), '5.2f'),	# Numeric value formated with width 5 and 2 decimal places
-            unicodedata.name(char),
-            sep='\t')
+    print(
+        "U+%04x" % ord(char),  # Code point in U+0000 format
+        char.center(6),  # Character centralized in a str of length 6
+        "re_dig" if re_digit.match(char) else "-",
+        "isdig" if char.isdigit() else "-",
+        "isnum" if char.isnumeric() else "-",
+        format(
+            unicodedata.numeric(char), "5.2f"
+        ),  # Numeric value formated with width 5 and 2 decimal places
+        unicodedata.name(char),
+        sep="\t",
+    )
 
-# The result shows that the regular expression r'\d' matches the digit “1” and the De‐ vanagari digit 3, but not some other characters that are considered digits by the isdi git function. The re module is not as savvy about Unicode as it could be.
+# The result shows that the regular expression r'\d' matches the digit “1” and the Devanagari digit 3, but not some other characters that are considered digits by the isdigit function. The re module is not as savvy about Unicode as it could be.
